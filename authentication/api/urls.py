d0fac91fe5f478f1eff76c51262f3db89157810a
trabnett <tim@singlekey.com>
2026-03-14 21:11:22 +1100

    add token refresh

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import re_path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     re_path(r'^welcome/?$', views.WelcomeView.as_view(), name='api-welcome'),
     re_path(r'^login/?$', views.PhoneLoginView.as_view(), name='api-phone-login'),
     re_path(r'^user/?$', views.UserProfileView.as_view(), name='api-user-profile'),
+    re_path(r'^token/refresh/?$', TokenRefreshView.as_view(), name='api-token-refresh'),
 ]
